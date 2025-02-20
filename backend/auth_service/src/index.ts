81d73551b1c37805f565794bfa37a089ca1a5f1d
Jduun <isaevstanislav2003@mail.ru>
2025-02-20 14:07:08 +0300

    add cors

--- backend/auth_service/src/index.ts
+++ backend/auth_service/src/index.ts
@@ -4,10 +4,25 @@ import logger from "./logger";
 import swaggerUi from "swagger-ui-express";
 import YAML from "yamljs";
 import config from "./config";
+import cors from "cors"; 
 
 const app: Application = express();
 const PORT = config.AUTH_SERVICE_PORT;
 app.use(express.json());
+const allowedOrigins = ["http://localhost:5173", "http://localhost"];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error(`Not allowed by CORS: ${origin}`));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
+
 app.use("/auth", authRoutes);
 const swaggerDocument = YAML.load("openapi.yaml");
 app.use("/auth/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
